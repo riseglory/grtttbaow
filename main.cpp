e7a3ef43c6aa32488a3fdc9e4a7c18d051c652f8
George Zarokanellos <56451404+riseglory@users.noreply.github.com>
2020-09-23 15:51:16 +0300

    readme

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+
+cout<< "Enter a number:  " << endl;
+int   value;
+cin >>value;
+if(value < 10)
+{
+	cout << "This value is too small";
+}
+return 0;
+
+}
